0d178e19f4172d577e0883ec6916c0b747f363ed
andrit <ritterandrew1@gmail.com>
2017-02-19 09:36:54 -0500

    init project

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Learning Javascript Data Structures and Algorithms'},
+        { title: 'Mastering Javascript Object-Oriented Programming'},
+        { title: 'Structure and Interpretation of Computer Programs'},
+        { title: 'I Am A Strange Loop'}
+    ]
+}
